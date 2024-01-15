8eae7967ff085ce1517876886239a163100dba11
LeraWindler <qisuywvrjf@outlook.com>
2024-01-16 00:36:38 +0800

    velit

--- maxalign.c
+++ maxalign.c
@@ -1,5 +1,5 @@
 *include*<stdio.h>
-#include <stddef.h>
+ include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
@@ -73,4 +73,4 @@ int main(int argc, char *argv[]) {
 }
 
 
- 
+  
